981f6df30439c1138e04e7355b9068e559d7e34b
Michael Groeneman <michael@groeneman.com>
2013-09-30 17:16:54 -0700

    Undo this unique thing

--- lib/reindex.rb
+++ lib/reindex.rb
@@ -4,22 +4,10 @@ class Reindex
   include Sidekiq::Worker
   
   sidekiq_options({
-    unique: true,
-    manual: true,
-    expiration: 60 # Lock lasts for 60 seconds, or until the indexing is performed.
+    queue: :reindex,
   })
-        
-  def self.lock *args
-    "locks:unique:#{args[0]}:#{args[1]}"
-  end
-  
-  def self.unlock! id, klazz
-    lock = self.lock(id,klazz)
-    Sidekiq.redis{|conn| conn.del(lock)}
-  end
-  
+
   def perform id, klazz
-    self.class.unlock!(id,klazz)
     klazz.constantize.find(id).index
   end
 end
